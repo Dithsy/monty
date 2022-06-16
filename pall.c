2663125a888f6c84e8015fcf3de1beda5a5aaa44
Eddith A <92625525+Dithsy@users.noreply.github.com>
2022-06-16 23:29:54 +0300

    mo

--- pall.c
+++ pall.c
@@ -1,13 +1,36 @@
 #include "monty.h"
 
 /**
- * pall - prints the stack
- * @stack: stack given by main
- * @line_cnt: ammount of lines
- *
- * Return: void
+ * set_op_tok_error - Sets last element of op_toks to be an error code.
+ * @error_code: Integer to store as a string in op_toks.
  */
-void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
+void set_op_tok_error(int error_code)
 {
-	print_stack(*stack);
+	int toks_len = 0, i = 0;
+	char *exit_str = NULL;
+	char **new_toks = NULL;
+
+	toks_len = token_arr_len();
+	new_toks = malloc(sizeof(char *) * (toks_len + 2));
+	if (!op_toks)
+	{
+		malloc_error();
+		return;
+	}
+	while (i < toks_len)
+	{
+		new_toks[i] = op_toks[i];
+		i++;
+	}
+	exit_str = get_int(error_code);
+	if (!exit_str)
+	{
+		free(new_toks);
+		malloc_error();
+		return;
+	}
+	new_toks[i++] = exit_str;
+	new_toks[i] = NULL;
+	free(op_toks);
+	op_toks = new_toks;
 }
