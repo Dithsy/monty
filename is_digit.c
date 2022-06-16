13b26d168f7d8418f1523f960a99cffa907c9088
Eddith A <92625525+Dithsy@users.noreply.github.com>
2022-06-16 23:18:27 +0300

    digit

--- is_digit.c
+++ is_digit.c
@@ -1,42 +1,95 @@
-#include <ctype.h>
-#include "monty.h"
+#include <stdlib.h>
+
+char *get_int(int num);
+unsigned int _abs(int);
+int get_numbase_len(unsigned int num, unsigned int base);
+void fill_numbase_buff(unsigned int num, unsigned int base,
+		       char *buff, int buff_size);
 
 /**
- * is_digit - checks if a string is a digit
- * @string: string to check
+ * get_int - gets a character pointer to new string containing int
+ * @num: number to convert to string
  *
- * Return: 1 if success, 0 if not
+ * Return: character pointer to newly created string. NULL if malloc fails.
  */
-int is_digit(char *string)
+char *get_int(int num)
 {
-	if (!string || *string == '\0')
-		return (0);
-	if (*string == '-')
-		string++;
-	while (*string)
-	{
-		if (isdigit(*string) == 0)
-			return (0);
-		string++;
-	}
-	return (1);
+	unsigned int temp;
+	int length = 0;
+	long num_l = 0;
+	char *ret;
+
+	temp = _abs(num);
+	length = get_numbase_len(temp, 10);
+
+	if (num < 0 || num_l < 0)
+		length++; /* negative sign */
+	ret = malloc(length + 1); /* create new string */
+	if (!ret)
+		return (NULL);
+
+	fill_numbase_buff(temp, 10, ret, length);
+	if (num < 0 || num_l < 0)
+		ret[0] = '-';
+
+	return (ret);
 }
+
 /**
- * isnumber - checks if a string is a number
- * @str: provided string
+ * _abs - gets the absolute value of an integer
+ * @i: integer to get absolute value of
  *
- * Return: 1 if the string is a number, else, 0.
+ * Return: unsigned integer abs rep of i
  */
-int isnumber(char *str)
+unsigned int _abs(int i)
 {
-	int i;
+	if (i < 0)
+		return (-(unsigned int)i);
+	return ((unsigned int)i);
+}
 
-	if (!str)
-		return (0);
+/**
+ * get_numbase_len - gets length of buffer needed for an unsigned int
+ * @num: number to get length needed for
+ * @base: base of number representation used by buffer
+ *
+ * Return: integer containing length of buffer needed (doesn't contain null bt)
+ */
+int get_numbase_len(unsigned int num, unsigned int base)
+{
+	int len = 1; /* all numbers contain atleast one digit */
 
-	for (i = 0; str[i]; i++)
-		if (i < '0' || i > '9')
-			return (0);
+	while (num > base - 1)
+	{
+		len++;
+		num /= base;
+	}
+	return (len);
+}
 
-	return (1);
+/**
+ * fill_numbase_buff - fills buffer with correct numbers up to base 36
+ * @num: number to convert to string given base
+ * @base: base of number used in conversion, only works up to base 36
+ * @buff: buffer to fill with result of conversion
+ * @buff_size: size of buffer in bytes
+ *
+ * Return: always void.
+ */
+void fill_numbase_buff(unsigned int num, unsigned int base,
+			char *buff, int buff_size)
+{
+	int rem, i = buff_size - 1;
+
+	buff[buff_size] = '\0';
+	while (i >= 0)
+	{
+		rem = num % base;
+		if (rem > 9) /* return lowercase ascii val representation */
+			buff[i] = rem + 87; /* 10 will be a, 11 = b, ... */
+		else
+			buff[i] = rem + '0';
+		num /= base;
+		i--;
+	}
 }
