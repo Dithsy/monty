aa3a72968fa1b3f3e1bf0f71ad3e689f94a61261
Eddith A <92625525+Dithsy@users.noreply.github.com>
2022-06-16 23:31:09 +0300

    stackk

--- swap.c
+++ swap.c
@@ -1,28 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "monty.h"
+#include <string.h>
+
+void free_stack(stack_t **stack);
+int init_stack(stack_t **stack);
+int check_mode(stack_t *stack);
 
 /**
- * swap -  swaps data from top to previous
- * @stack: stack given by main
- * @line_cnt: ammount of lines
- *
- * Return: void
+ * free_stack - Frees a stack_t stack.
+ * @stack: A pointer to the top (stack) or
+ *         bottom (queue) of a stack_t.
  */
-void swap(stack_t **stack, unsigned int line_cnt)
+void free_stack(stack_t **stack)
 {
-	stack_t *tmp = NULL;
-	int tmp_n = 0;
+	stack_t *tmp = *stack;
 
-	if (!stack || !*stack || !((*stack)->next))
+	while (*stack)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_cnt);
-		status = EXIT_FAILURE;
-		return;
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
 	}
-	tmp = *stack;
-	tmp_n = tmp->n;
-	tmp->n = tmp_n;
+}
 
+/**
+ * init_stack - Initializes a stack_t stack with beginning
+ *              stack and ending queue nodes.
+ * @stack: A pointer to an unitialized stack_t stack.
+ *
+ * Return: If an error occurs - EXIT_FAILURE.
+ *         Otherwise - EXIT_SUCCESS.
+ */
+int init_stack(stack_t **stack)
+{
+	stack_t *s;
+
+	s = malloc(sizeof(stack_t));
+	if (s == NULL)
+		return (malloc_error());
+
+	s->n = STACK;
+	s->prev = NULL;
+	s->next = NULL;
+
+	*stack = s;
+
+	return (EXIT_SUCCESS);
+}
+
+/**
+ * check_mode - Checks if a stack_t linked list is in stack or queue mode.
+ * @stack: A pointer to the top (stack) or bottom (queue)
+ *         of a stack_t linked list.
+ *
+ * Return: If the stack_t is in stack mode - STACK (0).
+ *         If the stack_t is in queue mode - QUEUE (1).
+ *         Otherwise - 2.
+ */
+int check_mode(stack_t *stack)
+{
+	if (stack->n == STACK)
+		return (STACK);
+	else if (stack->n == QUEUE)
+		return (QUEUE);
+	return (2);
 }
